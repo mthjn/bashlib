a311df05d678948614761724acedeed19342cea9
mthjn <mthjn@users.noreply.github.com>
2016-09-14 09:49:31 +0200

    Update netstat.sh

--- slacktee/netstat.sh
+++ slacktee/netstat.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-  echo "Do it as root" 1>&2
-  exit 1
-fi
-
-set -ofu pipefail
-
-netstat -taupen | slacktee.sh
+netstat -tauen | /usr/local/bin/slacktee.sh
